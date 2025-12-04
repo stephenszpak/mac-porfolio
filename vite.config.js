dd8eb3ba37a5fcfa7b2562c7750c7b36e569f49a
Stephen Szpak <stephenszpak@Stephens-MacBook-Air.local>
2025-12-04 10:37:16 -0600

    update workflow 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/').pop() : ''
+const isPages = !!process.env.GITHUB_PAGES || process.env.CI === 'true'
+
 export default defineConfig({
   plugins: [react()],
+  base: isPages && repo ? `/${repo}/` : '/',
 })
 
