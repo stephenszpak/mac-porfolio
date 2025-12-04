04520120850e995e9c6401c86807446b113a7185
Stephen Szpak <stephenszpak@Stephens-MacBook-Air.local>
2025-12-04 13:30:06 -0600

    maybe fix deploy?

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 const repo = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/').pop() : ''
 const isPages = !!process.env.GITHUB_PAGES || process.env.CI === 'true'
+const explicitBase = process.env.VITE_BASE
 
 export default defineConfig({
   plugins: [react()],
-  base: isPages && repo ? `/${repo}/` : '/',
+  base: explicitBase ?? (isPages && repo ? `/${repo}/` : '/'),
 })
-
